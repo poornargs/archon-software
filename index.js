6d8c308867b148e719fd665cb86a41741bad0b67
poornargs <42318980+poornargs@users.noreply.github.com>
2023-02-12 23:07:30 +0530

    removed dotenv

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 var express = require("express");
 const cors = require('cors');
 
-require('dotenv').config()
+// require('dotenv').config()
 
 // console.log(process.env)
 
-let { SERVER_PORT } = process.env;
+// let { SERVER_PORT } = process.env;
 
 
-SERVER_PORT = SERVER_PORT || 6655;
+// SERVER_PORT = SERVER_PORT || 6655;
 
-// let SERVER_PORT = 6655;
+let SERVER_PORT = 6655;
 
 // Initialize Express
 var app = express();
@@ -28,6 +28,6 @@ app.use(express.static("archon"));
 //     console.log("Listening on port " + SERVER_PORT + ".");
 // });
 
-app.listen(port, process.env.HOST || "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}, host on ${JSON.stringify(app.listen().address())}`)
 })
